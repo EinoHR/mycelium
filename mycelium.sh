1faa24526b304124d6b53e6754036b593d22f628
EinoHR <eino.rauhala@protonmail.com>
2023-03-11 12:58:15 +0200

    shell script syntax???

--- mycelium.sh
+++ mycelium.sh
@@ -5,14 +5,14 @@ xset s off
         echo "Dbclient crashed with exit code $?.  Respawning.." >&2
         sleep 1
     done
-)& &> /var/home/mycelium/dbclient.log
+) &> /var/home/mycelium/dbclient.log &
 
 (
     until /usr/lib/jvm/jre-11-openjdk-11.0.18.0.10-1.fc37.x86_64/bin/java -Djava.io.tmpdir=/var/home/mycelium -jar mycelium.jar; do
         echo "Java crashed with exit code $?.  Respawning.." >&2
         sleep 1
     done
-)& &> /var/home/mycelium/openrndr.log
+) &> /var/home/mycelium/openrndr.log &
 
 sleep 2
 execstack -c /var/home/mycelium/.jSerialComm/2.9.3/libjSerialComm.so
