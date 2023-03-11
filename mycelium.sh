ed08a2b3b4397136a58dd69ba5bca8cfd01e7c66
EinoHR <eino.rauhala@protonmail.com>
2023-03-11 14:15:06 +0200

    rm java tmpdir

--- mycelium.sh
+++ mycelium.sh
@@ -8,7 +8,7 @@ xset s off
 ) &> /var/home/mycelium/dbclient.log &
 
 (
-    until /usr/lib/jvm/jre-11-openjdk-11.0.18.0.10-1.fc37.x86_64/bin/java -Djava.io.tmpdir=/var/home/mycelium -jar mycelium.jar; do
+    until /usr/lib/jvm/jre-11-openjdk-11.0.18.0.10-1.fc37.x86_64/bin/java -jar mycelium.jar; do
         echo "Java crashed with exit code $?.  Respawning.." >&2
         sleep 1
     done
