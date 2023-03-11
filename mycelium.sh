ad7683ebb97fa12d840a54bd6589c40df80e41d7
EinoHR <eino.rauhala@protonmail.com>
2023-03-11 12:07:09 +0200

    don't change folder

--- mycelium.sh
+++ mycelium.sh
@@ -1,7 +1,5 @@
 xset s off
 
-cd /usr/mycelium
-
 (
     (
         until ./dbclient; do
@@ -10,7 +8,7 @@ cd /usr/mycelium
         done
     )&
 
-    until /usr/lib/jvm/jre-11-openjdk-11.0.18.0.10-1.fc37.x86_64/bin/java -Djava.io.tmpdir=/var/mycelium -jar mycelium.jar; do
+    until /usr/lib/jvm/jre-11-openjdk-11.0.18.0.10-1.fc37.x86_64/bin/java -Djava.io.tmpdir=/var/home/mycelium -jar mycelium.jar; do
         echo "Java crashed with exit code $?.  Respawning.." >&2
         sleep 1
     done
