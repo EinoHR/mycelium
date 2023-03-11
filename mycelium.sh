ef238076eb89d5941697a95691a34b3f4efbc5e6
EinoHR <eino.rauhala@protonmail.com>
2023-03-11 12:29:56 +0200

    split up logging

--- mycelium.sh
+++ mycelium.sh
@@ -1,18 +1,18 @@
 xset s off
 
 (
-    (
-        until ./dbclient; do
-            echo "Dbclient crashed with exit code $?.  Respawning.." >&2
-            sleep 1
-        done
-    )&
+    until ./dbclient; do
+        echo "Dbclient crashed with exit code $?.  Respawning.." >&2
+        sleep 1
+    done
+)& &> /var/home/mycelium/dbclient.log
 
+(
     until /usr/lib/jvm/jre-11-openjdk-11.0.18.0.10-1.fc37.x86_64/bin/java -Djava.io.tmpdir=/var/home/mycelium -jar mycelium.jar; do
         echo "Java crashed with exit code $?.  Respawning.." >&2
         sleep 1
     done
-) &> /var/home/mycelium/mycelium.log
+)& &> /var/home/mycelium/openrndr.log
 
 sleep 2
 execstack -c /var/home/mycelium/.jSerialComm/2.9.3/libjSerialComm.so
